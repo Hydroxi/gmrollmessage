2dde6a45a8ca657c4981593713ceff7fb0e2bf77
d.laun <d.laun@freesixtyfive.de>
2019-07-24 16:59:16 +0200

    fix js

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
 class GmRollMessage extends Application {
     constructor() {
         super();
-
+    }
+    init() {
         Hooks.on("chatMessage", (app, html, data) => {
             console.log(data);
-            if ((data.type === "gmroll") || (data.type === "blindroll")) {
-                AudioHelper.play({src: CONFIG.sounds.dice});
+            if (data.type === "gmroll" || data.type === "blindroll") {
+                AudioHelper.play({ src: CONFIG.sounds.dice });
                 ChatMessage.create({
                     user: game.user._id,
                     speaker: { actor: this.actor },
@@ -16,4 +17,5 @@ class GmRollMessage extends Application {
     }
 }
 
-let app = new GmRollMessage();
+let gmRollMessage = new GmRollMessage();
+gmRollMessage.init();
